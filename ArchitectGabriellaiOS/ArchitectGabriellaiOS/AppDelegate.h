fc9bf7224584f17542f9824213dca6edc9845396
Gabriellamm <379713491@qq.com>
2017-06-01 00:18:15 +0800

    init

---
+++ ArchitectGabriellaiOS/ArchitectGabriellaiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArchitectGabriellaiOS
+//
+//  Created by Gabriella on 2017/6/1.
+//  Copyright © 2017年 Gabriella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
